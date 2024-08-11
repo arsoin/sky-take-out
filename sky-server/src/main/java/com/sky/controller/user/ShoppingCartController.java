493f7bc7327ef23898a7977ebdd1d80846d6a385
kikyo <hamehamepa@outlook.com>
2024-08-11 19:55:03 +0900

    查看购物车代码开发

--- sky-server/src/main/java/com/sky/controller/user/ShoppingCartController.java
+++ sky-server/src/main/java/com/sky/controller/user/ShoppingCartController.java
@@ -8,10 +8,9 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/user/shoppingCart")
@@ -33,4 +32,18 @@ public class ShoppingCartController {
         shoppingCartService.addShoppingCart(shoppingCartDTO);
         return Result.success();
     }
+
+    /**
+     * 查看购物车
+     * @return
+     */
+    @ApiOperation(value = "查看购物车")
+    @GetMapping("/list")
+    public Result<List<ShoppingCart>> list(){
+        log.info("查看购物车");
+        List<ShoppingCart> list =  shoppingCartService.showShoppingCart();
+        return Result.success(list);
+    }
+
+
 }
